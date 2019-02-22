b6ee179cfbfa7a41c8b473754289f523aeba85db
dezhidki <denis.zhidkikh@gmail.com>
2019-02-22 21:25:46 +0200

    Add subkey parsing; reformat document

--- Tommy/TOML.cs
+++ Tommy/TOML.cs
@@ -7,11 +7,13 @@ namespace Tommy
 {
     public class TomlNode
     {
+        private string _rawValue;
         private Dictionary<string, TomlNode> children;
-        public string RawValue { get; set; }
+        public Dictionary<string, TomlNode> Children => children ?? (children = new Dictionary<string, TomlNode>());
+
+        public bool HasValue { get; protected set; }
 
         public bool IsTable { get; protected set; }
-        public Dictionary<string, TomlNode> Children => children ?? (children = new Dictionary<string, TomlNode>());
 
         public TomlNode this[string key]
         {
@@ -19,10 +21,17 @@ namespace Tommy
             set => Children[key] = value;
         }
 
-        public static implicit operator TomlNode(string str) => new TomlNode
+        public string RawValue
         {
-            RawValue = str
-        };
+            get => _rawValue;
+            set
+            {
+                HasValue = true;
+                _rawValue = value;
+            }
+        }
+
+        public static implicit operator TomlNode(string str) => new TomlNode {RawValue = str};
     }
 
     public class TomlTable : TomlNode
@@ -32,13 +41,102 @@ namespace Tommy
 
     public static class TOML
     {
-        private const char COMMENT_SYMBOL = '#';
-        private const char KEY_VALUE_SEPARATOR = '=';
-        private const char NEWLINE_CHARACTER = '\n';
-        private const char NEWLINE_CARRIAGE_RETURN_CHARACTER = '\r';
-        private const char SUBKEY_SEPARATOR = '.';
-        private const char ESCAPE_SYMBOL = '\\';
         private const char BASIC_STRING_SYMBOL = '\"';
+        private const char COMMENT_SYMBOL = '#';
+        private const char ESCAPE_SYMBOL = '\\';
+        private const char KEY_VALUE_SEPARATOR = '=';
+        private const char NEWLINE_CARRIAGE_RETURN_CHARACTER = '\r';
+        private const char NEWLINE_CHARACTER = '\n';
+        private const char SUBKEY_SEPARATOR = '.';
+
+        public static TomlNode Parse(TextReader reader)
+        {
+            var result = new TomlNode();
+
+            var currentNode = result;
+
+            var state = ParseState.None;
+
+            var keyParts = new List<string>();
+
+            int currentChar;
+            while ((currentChar = reader.Read()) >= 0)
+            {
+                char c = (char) currentChar;
+
+                if (state == ParseState.None)
+                {
+                    // Skip white space
+                    if (IsWhiteSpace(c) || IsNewLine(c))
+                        continue;
+
+                    // Start of a comment; ignore until newline
+                    if (c == COMMENT_SYMBOL)
+                    {
+                        reader.ReadLine();
+                        continue;
+                    }
+
+                    //TODO: Tables
+
+                    //TODO: Array tables
+
+                    if (IsBareKey(c) || IsQuoted(c))
+                        state = ParseState.Key;
+                    else
+                        throw new Exception($"Unexpected character \"{c}\"");
+                }
+
+                if (state == ParseState.Key)
+                {
+                    if (IsQuoted(c) || IsBareKey(c))
+                    {
+                        if (keyParts.Count != 0)
+                            throw new Exception("Encountered extra characters in key definition!");
+
+                        ReadKeyName(reader, ref keyParts, c);
+                        continue;
+                    }
+
+                    if (IsWhiteSpace(c))
+                        continue;
+
+                    if (c == KEY_VALUE_SEPARATOR)
+                    {
+                        state = ParseState.Value;
+                        continue;
+                    }
+
+                    throw new Exception("Invalid character in key!");
+                }
+
+                if (state == ParseState.Value)
+                {
+                    if (IsWhiteSpace(c))
+                        continue;
+
+                    if (IsQuoted(c))
+                    {
+                        string value = IsTripleQuote(c, reader, out string excess)
+                                               ? ReadQuotedValueMultiLine(c, reader)
+                                               : ReadQuotedValueSingleLine(c, reader, excess);
+
+                        var node = CreateValueNode(currentNode, keyParts);
+                        node.RawValue = value;
+                        keyParts.Clear();
+                    }
+
+                    // TODO: Numbers, Dates, Booleans, Lists, Inline Tables
+
+                    if (c == COMMENT_SYMBOL)
+                        throw new Exception("The key has no value!");
+
+                    state = ParseState.None;
+                }
+            }
+
+            return result;
+        }
 
         private static bool IsQuoted(char c) => c == BASIC_STRING_SYMBOL || c == '\'';
 
@@ -48,16 +146,13 @@ namespace Tommy
 
         private static bool IsEmptySpace(char c) => IsWhiteSpace(c) || IsNewLine(c);
 
-        private static bool IsBareKey(char c) => 'A' <= c && c <= 'Z' ||
-                                                 'a' <= c && c <= 'z' ||
-                                                 '0' <= c && c <= '9' ||
-                                                 c == '_' ||
-                                                 c == '-';
+        private static bool IsBareKey(char c) =>
+                'A' <= c && c <= 'Z' || 'a' <= c && c <= 'z' || '0' <= c && c <= '9' || c == '_' || c == '-';
 
         private static bool IsTripleQuote(char quote, TextReader reader, out string excess)
         {
             var buffer = new char[2];
-            var read = reader.ReadBlock(buffer, 0, 2);
+            int read = reader.ReadBlock(buffer, 0, 2);
 
             if (read == 2 && buffer[0] == quote && buffer[1] == quote)
             {
@@ -69,12 +164,7 @@ namespace Tommy
             return false;
         }
 
-        private static bool ProcessQuotedValueCharacter(char quote,
-                                                        bool isBasic,
-                                                        char c,
-                                                        int next,
-                                                        StringBuilder sb,
-                                                        ref bool escaped)
+        private static bool ProcessQuotedValueCharacter(char quote, bool isBasic, char c, int next, StringBuilder sb, ref bool escaped)
         {
             if (escaped)
             {
@@ -97,27 +187,83 @@ namespace Tommy
             return false;
         }
 
+        private static void ReadKeyName(TextReader tr, ref List<string> parts, char firstChar)
+        {
+            var buffer = new StringBuilder();
+
+            bool quoted = IsQuoted(firstChar);
+
+            if (quoted)
+                buffer.Append(ReadQuotedValueSingleLine(firstChar, tr, null));
+            else
+                buffer.Append(firstChar);
+
+            int cur;
+            while ((cur = tr.Read()) >= 0)
+            {
+                char c = (char) cur;
+
+                // Stop if we see whitespace in non-quoted context; let main parser cause the possible error
+                if (IsWhiteSpace(c) || c == KEY_VALUE_SEPARATOR)
+                    break;
+
+                if (c == SUBKEY_SEPARATOR)
+                {
+                    if (buffer.Length == 0)
+                        throw new Exception("No subkey identified!");
+
+                    parts.Add(buffer.ToString());
+                    buffer.Length = 0;
+                    quoted = false;
+                    continue;
+                }
+
+                if (IsQuoted(c))
+                {
+                    if (quoted)
+                        throw new Exception("Expected a subkey separator but got extra data instead!");
+                    if (buffer.Length != 0)
+                        throw new Exception("Encountered a premature quote!");
+
+                    buffer.Append(ReadQuotedValueSingleLine(c, tr, null));
+                    quoted = true;
+                    continue;
+                }
+
+                if (IsBareKey(c))
+                {
+                    buffer.Append(c);
+                    continue;
+                }
+
+                throw new Exception("Encountered an invalid symbol in key definition!");
+            }
+
+            parts.Add(buffer.ToString());
+        }
+
         private static string ReadQuotedValueSingleLine(char quote, TextReader reader, string initialData)
         {
-            var isBasic = quote == BASIC_STRING_SYMBOL;
+            bool isBasic = quote == BASIC_STRING_SYMBOL;
             var sb = new StringBuilder();
 
-            var escaped = false;
+            bool escaped = false;
 
             // Catch up with possible initial data
-            for (var i = 0; i < initialData.Length; i++)
-                if (ProcessQuotedValueCharacter(quote,
-                                                isBasic,
-                                                initialData[i],
-                                                i < initialData.Length - 1 ? initialData[i + 1] : -1,
-                                                sb,
-                                                ref escaped))
-                    return isBasic ? sb.ToString().Unescape() : sb.ToString();
+            if (initialData != null)
+                for (int i = 0; i < initialData.Length; i++)
+                    if (ProcessQuotedValueCharacter(quote,
+                                                    isBasic,
+                                                    initialData[i],
+                                                    i < initialData.Length - 1 ? initialData[i + 1] : -1,
+                                                    sb,
+                                                    ref escaped))
+                        return isBasic ? sb.ToString().Unescape() : sb.ToString();
 
             int cur;
             while ((cur = reader.Read()) >= 0)
             {
-                var c = (char) cur;
+                char c = (char) cur;
                 if (ProcessQuotedValueCharacter(quote, isBasic, c, reader.Peek(), sb, ref escaped))
                     break;
             }
@@ -127,18 +273,18 @@ namespace Tommy
 
         private static string ReadQuotedValueMultiLine(char quote, TextReader reader)
         {
-            var isBasic = quote == BASIC_STRING_SYMBOL;
+            bool isBasic = quote == BASIC_STRING_SYMBOL;
             var sb = new StringBuilder();
 
-            var escaped = false;
-            var skipWhitespace = false;
-            var quotesEncountered = 0;
-            var first = true;
+            bool escaped = false;
+            bool skipWhitespace = false;
+            int quotesEncountered = 0;
+            bool first = true;
 
             int cur;
             while ((cur = reader.Read()) >= 0)
             {
-                var c = (char) cur;
+                char c = (char) cur;
 
                 // Trim the first newline
                 if (first && IsNewLine(c))
@@ -167,7 +313,7 @@ namespace Tommy
                 // If we encounter an escape sequence...
                 if (c == ESCAPE_SYMBOL)
                 {
-                    var next = reader.Peek();
+                    int next = reader.Peek();
                     if (next >= 0)
                     {
                         // ...and the next char is empty space, we must skip all whitespaces
@@ -202,111 +348,29 @@ namespace Tommy
             return isBasic ? sb.ToString().Unescape() : sb.ToString();
         }
 
-        public static TomlNode Parse(TextReader reader)
+        private static TomlNode CreateValueNode(TomlNode root, List<string> path)
         {
-            var result = new TomlNode();
+            var latestNode = root;
 
-            var currentNode = result;
-
-            var state = ParseState.None;
-
-            var buffer = new StringBuilder();
-            var key = string.Empty;
-
-            int currentChar;
-            while ((currentChar = reader.Read()) >= 0)
+            foreach (string subkey in path)
             {
-                var c = (char) currentChar;
-
-                if (state == ParseState.None)
+                if (latestNode.Children.TryGetValue(subkey, out var node))
                 {
-                    // Skip white space
-                    if (IsWhiteSpace(c) || IsNewLine(c))
-                        continue;
-
-                    // Start of a comment; ignore until newline
-                    if (c == COMMENT_SYMBOL)
-                    {
-                        reader.ReadLine();
-                        continue;
-                    }
-
-                    //TODO: Tables
-
-                    //TODO: Array tables
-
-                    if (IsBareKey(c))
-                    {
-                        state = ParseState.Key;
-                        buffer.Append(c);
-                        continue;
-                    }
-
-                    throw new Exception($"Unexpected character \"{c}\"");
+                    if (node.HasValue)
+                        throw new Exception("The key already has a value assigned to it!");
+                    if (node.IsTable)
+                        throw new Exception("The key is a table and thus is not a valid value");
+                }
+                else
+                {
+                    node = new TomlNode();
+                    latestNode[subkey] = node;
                 }
 
-                if (state == ParseState.Key)
-                {
-                    // TODO: Subkey
-
-                    if (IsQuoted(c))
-                    {
-                        //TODO: Quoted key
-                    }
-
-                    if (c == SUBKEY_SEPARATOR)
-                    {
-                        //TODO: Separator
-                    }
-
-                    if (IsBareKey(c))
-                    {
-                        buffer.Append(c);
-                        continue;
-                    }
-
-                    if (IsWhiteSpace(c))
-                        continue;
-
-                    if (c == KEY_VALUE_SEPARATOR)
-                    {
-                        state = ParseState.Value;
-
-                        key = buffer.ToString();
-                        buffer.Length = 0;
-
-                        continue;
-                    }
-
-                    throw new Exception("Invalid character in key!");
-                }
-
-                if (state == ParseState.Value)
-                {
-                    if (IsWhiteSpace(c))
-                        continue;
-
-                    if (IsQuoted(c))
-                    {
-                        var value = IsTripleQuote(c, reader, out var excess)
-                            ? ReadQuotedValueMultiLine(c, reader)
-                            : ReadQuotedValueSingleLine(c, reader, excess);
-                        currentNode[key] = new TomlNode
-                        {
-                            RawValue = value
-                        };
-
-                        key = string.Empty;
-                    }
-
-                    if (c == COMMENT_SYMBOL)
-                        throw new Exception("The key has no value!");
-
-                    state = ParseState.None;
-                }
+                latestNode = node;
             }
 
-            return result;
+            return latestNode;
         }
 
         private enum ParseState
@@ -324,15 +388,15 @@ namespace Tommy
             if (string.IsNullOrEmpty(txt))
                 return txt;
             var stringBuilder = new StringBuilder(txt.Length);
-            for (var i = 0; i < txt.Length;)
+            for (int i = 0; i < txt.Length;)
             {
-                var num = txt.IndexOf('\\', i);
+                int num = txt.IndexOf('\\', i);
                 if (num < 0 || num == txt.Length - 1)
                     num = txt.Length;
                 stringBuilder.Append(txt, i, num - i);
                 if (num >= txt.Length)
                     break;
-                var c = txt[num + 1];
+                char c = txt[num + 1];
                 switch (c)
                 {
                     case 'b':
