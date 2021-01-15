bcf765001f7a542b12622c1b178ab276e010a256
dezhidki <me@deni.sh>
2021-01-15 21:59:44 +0200

    Add initial parsing tests from TOML docs

--- Tommy.Tests/ParseTests.cs
+++ Tommy.Tests/ParseTests.cs
@@ -10,8 +10,74 @@ namespace Tommy.Tests
 {
     public class ParseTests
     {
+        private static readonly ParseTestCollection BasicTests = new()
+        {
+            new(@"
+            # This is a full-line comment
+            key = ""value""  # This is a comment at the end of a line
+            another = ""# This is not a comment""
+            ",
+                @"{""key"":""value"",""another"":""# This is not a comment""}")
+        };
+
+        private static readonly ParseTestCollection KeysTests = new()
+        {
+            new(@"
+            key = ""value""
+            bare_key = ""value""
+            bare-key = ""value""
+            1234 = ""value""
+            ",
+                @"{""key"":""value"",""bare_key"":""value"",""bare-key"":""value"",""1234"":""value""}"),
+            new(@"
+            ""127.0.0.1"" = ""value""
+            ""character encoding"" = ""value""
+            ""ʎǝʞ"" = ""value""
+            'key2' = ""value""
+            'quoted ""value""' = ""value""
+            ",
+                @"{""127.0.0.1"":""value"",""character encoding"":""value"",""ʎǝʞ"":""value"",""key2"":""value"",""quoted \""value\"""":""value""}"),
+            new(@"
+            name = ""Orange""
+            physical.color = ""orange""
+            physical.shape = ""round""
+            site.""google.com"" = true
+            ",
+                @"{""name"":""Orange"",""physical"":{""color"":""orange"",""shape"":""round""},""site"":{""google.com"":true}}"),
+            new(@"
+            fruit.name = ""banana""     # this is best practice
+            fruit. color = ""yellow""    # same as fruit.color
+            fruit . flavor = ""banana""   # same as fruit.flavor
+            ",
+                @"{""fruit"":{""name"":""banana"",""color"":""yellow"",""flavor"":""banana""}}"),
+            new(@"
+            # This makes the key ""fruit"" into a table.
+            fruit.apple.smooth = true
+
+            # So then you can add to the table ""fruit"" like so:
+            fruit.orange = 2
+            ",
+                @"{""fruit"":{""apple"":{""smooth"":true},""orange"":2}}"),
+            new(@"
+            apple.type = ""fruit""
+            orange.type = ""fruit""
+
+            apple.skin = ""thin""
+            orange.skin = ""thick""
+
+            apple.color = ""red""
+            orange.color = ""orange""
+            ",
+                @"{""apple"":{""type"":""fruit"",""skin"":""thin"",""color"":""red""},""orange"":{""type"":""fruit"",""skin"":""thick"",""color"":""orange""}}"),
+            new(@"
+            3.14159 = ""pi""
+            ",
+                @"{""3"":{""14159"":""pi""}}")
+        };
+
         [Test]
         [TestCaseSource(nameof(BasicTests), Category = "Basic tests")]
+        [TestCaseSource(nameof(KeysTests), Category = "Key tests")]
         public void ParsePositiveTest(string toml, string expectedJson)
         {
             Console.WriteLine(toml);
@@ -32,14 +98,5 @@ namespace Tommy.Tests
             var json = tomlNode.ToJsonString();
             Assert.AreEqual(expectedJson, json);
         }
-
-        private static readonly ParseTestCollection BasicTests = new()
-        {
-            new(@"
-            # This is a full-line comment
-            key = ""value""  # This is a comment at the end of a line
-            another = ""# This is not a comment""
-            ", @"{""key"":""value"",""another"":""# This is not a comment""}")
-        };
     }
 }
