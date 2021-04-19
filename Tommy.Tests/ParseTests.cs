517d01f4ceb30da8221c7237fa8097d8523bd97d
dezhidki <me@deni.sh>
2021-04-19 22:00:21 +0300

    Add invalid parse tests

--- Tommy.Tests/ParseTests.cs
+++ Tommy.Tests/ParseTests.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -41,6 +42,19 @@ namespace Tommy.Tests
             Assert.AreEqual(expectedJson, json);
         }
 
+        [TestCaseSource(nameof(TestParseFailure), new object[] {"array"}, Category = "Array invalid parse tests")]
+        [TestCaseSource(nameof(TestParseFailure), new object[] {"comment"}, Category = "Comment invalid parse tests")]
+        [TestCaseSource(nameof(TestParseFailure), new object[] {"integer"}, Category = "Integer invalid parse tests")]
+        [TestCaseSource(nameof(TestParseFailure), new object[] {"key-value"}, Category = "Key-value invalid parse tests")]
+        [TestCaseSource(nameof(TestParseFailure), new object[] {"string"}, Category = "String invalid parse tests")]
+        [TestCaseSource(nameof(TestParseFailure), new object[] {"table"}, Category = "Table invalid parse tests")]
+        public void ParseNegativeTest(FailureTest test) =>
+            Assert.Catch<TomlParseException>(() =>
+            {
+                var table = TOML.Parse(new StringReader(test.Toml));
+                Console.WriteLine(table.ToString());
+            });
+
         private static IEnumerable<SuccessTest> TestParseSuccess(string caseSetName)
         {
             var casesPath = Path.Combine("cases", "valid", caseSetName);
@@ -50,6 +64,19 @@ namespace Tommy.Tests
                 var jsonFile = Path.Combine(casesPath, $"{testName}.json");
                 yield return new SuccessTest(File.ReadAllText(tomlFile), File.ReadAllText(jsonFile), $"{caseSetName}/{testName}");
             }
+        }
+
+        private static IEnumerable<FailureTest> TestParseFailure(string caseSetName)
+        {
+            var casesPath = Path.Combine("cases", "invalid", caseSetName);
+            foreach (var tomlFile in Directory.EnumerateFiles(casesPath, "*.toml"))
+                yield return new FailureTest(File.ReadAllText(tomlFile),
+                                             $"{caseSetName}/{Path.GetFileNameWithoutExtension(tomlFile)}");
+        }
+
+        public record FailureTest(string Toml, string TestName)
+        {
+            public override string ToString() => TestName;
         }
 
         public record SuccessTest(string Toml, string Json, string TestName)
