fc89e168cfb886ca3426746e66414bd70f35dd44
js6pak <kubastaron@hotmail.com>
2021-06-21 18:14:14 +0200

    Tommy.Extensions.Configuration

---
+++ Tommy.Extensions.Configuration/TomlConfigurationSource.cs
@@ -0,0 +1,21 @@
+using Microsoft.Extensions.Configuration;
+
+namespace Tommy.Extensions.Configuration
+{
+    /// <summary>
+    /// Represents a TOML file as an <see cref="IConfigurationSource"/>.
+    /// </summary>
+    public class TomlConfigurationSource : FileConfigurationSource
+    {
+        /// <summary>
+        /// Builds the <see cref="TomlConfigurationProvider"/> for this source.
+        /// </summary>
+        /// <param name="builder">The <see cref="IConfigurationBuilder"/>.</param>
+        /// <returns>A <see cref="TomlConfigurationProvider"/></returns>
+        public override IConfigurationProvider Build(IConfigurationBuilder builder)
+        {
+            EnsureDefaults(builder);
+            return new TomlConfigurationProvider(this);
+        }
+    }
+}
