9a0a1b456db1324afc1ff5e7e5149d8ca755170f
Konrad <konrad.kogut@outlook.com>
2022-01-06 16:59:29 +0100

    AddTomlStream

---
+++ Tommy.Extensions.Configuration/TomlStreamConfigurationProvider.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+
+namespace Tommy.Extensions.Configuration
+{
+    /// <summary>
+    /// Loads configuration key/values from a TOML stream into a provider.
+    /// </summary>
+    public class TomlStreamConfigurationProvider : StreamConfigurationProvider
+    {
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        /// <param name="source">The <see cref="TomlStreamConfigurationSource"/>.</param>
+        public TomlStreamConfigurationProvider(TomlStreamConfigurationSource source) : base(source) { }
+    
+        /// <summary>
+        /// Loads TOML configuration key/values from a stream into a provider.
+        /// </summary>
+        /// <param name="stream">The TOML <see cref="Stream"/> to load configuration data from.</param>
+        public override void Load(Stream stream) => Data = TomlConfigurationFileParser.Parse(stream);
+    }
+}
---
+++ Tommy.Extensions.Configuration/TomlStreamConfigurationSource.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace Tommy.Extensions.Configuration
+{
+    /// <summary>
+    /// Represents a TOML file as an <see cref="IConfigurationSource"/>.
+    /// </summary>
+    public class TomlStreamConfigurationSource : StreamConfigurationSource
+    {
+        /// <summary>
+        /// Builds the <see cref="TomlStreamConfigurationProvider"/> for this source.
+        /// </summary>
+        /// <param name="builder">The <see cref="IConfigurationBuilder"/>.</param>
+        /// <returns>An <see cref="TomlStreamConfigurationProvider"/></returns>
+        public override IConfigurationProvider Build(IConfigurationBuilder builder)
+            => new TomlStreamConfigurationProvider(this);
+    }
+}
